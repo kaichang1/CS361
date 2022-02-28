6680a0083133bbd10b3a2366f078acaa571148d5
Kai <kaichang92@gmail.com>
2022-02-27 20:03:22 -0800

    Integrate teammate's microservice

---
+++ App.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import main
+import time
+import os
+
+app = Flask(__name__)
+app.config.from_pyfile("config.py")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/analyze", methods=["POST", "GET"])
+def analyze():
+    if request.method == "POST":
+        url = request.form["url"]
+        return redirect(url_for("results", url=url))
+    else:
+        news = main.news_scraper()
+        return render_template("analyze.html", news=news)
+
+@app.route("/results")
+def results():
+    url = request.args.get("url")
+    # If user attempts to directly go to /results
+    if url is None:
+        return redirect(url_for("analyze"))
+    result = main.article_scraper(url)
+
+    # If the scraper was unsuccessful
+    if result is None:
+        flash("Oops! It looks like that page is in an unsupported format. Please try again and ensure that you are entering a MarketWatch article.")
+        return redirect(url_for("analyze"))
+
+    title, body = result
+    entire_article = title + " " + body
+
+    counts_table = main.get_counts_df(entire_article)
+    subjectivity = main.get_subjectivity(entire_article)
+    polarity = main.get_polarity(entire_article)
+    polarizing_sentences = main.get_polarizing_sentences(entire_article)
+
+    if counts_table is not None:
+        main_company = counts_table.iloc[0]["company"]
+        main_symbol = counts_table.iloc[0]["symbol"]
+        counts_table = counts_table.to_html(index=False, justify="left", classes="table table-striped")
+    else:
+        main_company = None
+        main_symbol = None
+
+    # -------------------------------------- #
+    # Interacting with teammate's microservice
+    # -------------------------------------- #
+    print("Sending request...")
+    with open('../361-microservice-main/ticker-input.txt', 'w') as file:
+        file.write(main_symbol)
+    print("Request sent")
+
+    while True:
+        print("Waiting for response...")
+        try:
+            with open('../361-microservice-main/ticker-output.txt') as file:
+                microservice_response = file.read()
+        except FileNotFoundError:
+            time.sleep(2)
+        else:
+            print("Response retrieved")
+            os.remove('../361-microservice-main/ticker-output.txt')
+            break
+    # -------------------------------------- #
+
+    return render_template("results.html", url=url, title=title, counts_table=counts_table, main_company=main_company, main_symbol=main_symbol, subjectivity=subjectivity, polarity=polarity, polarizing_sentences=polarizing_sentences, microservice_response=microservice_response)
+    
+    # -------------------------------------- #
+
+@app.route("/help")
+def help():
+    return render_template("help.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -4,8 +4,37 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
 import requests
 from bs4 import BeautifulSoup
-import time
-from PIL import Image
+
+
+def news_scraper():
+    news = []
+    url = "https://www.marketwatch.com/investing?mod=top_nav"
+
+    try:
+        webpage = requests.get(url)
+    except Exception as err:
+        print("Error!")
+        print(f"Message: {err}")
+        print(f"Type: {type(err)}")
+        return None
+
+    soup = BeautifulSoup(webpage.content, 'html.parser')
+
+    # Don't think we need this since we aren't doing .text() or something but I'll leave it here for now just in case
+    # try:
+    # except AttributeError:
+    #     print("attribute")
+    #     return None
+
+    articles = soup.find(attrs={'class': 'collection__elements j-scrollElement'})
+    articles = articles.find_all(attrs={'class': 'element--article'})
+
+    for article in articles:
+        link = article.find(attrs={'class': 'link'})
+        if link:
+            news.append(link)
+
+    return news
 
 
 def article_scraper(url):
@@ -47,7 +76,6 @@ def get_counts_df(text):
     doc = nlp(text)
 
     for ent in doc.ents:
-        # print(ent.text, ent.label_)
         if ent.label_ == 'Stock':
             counts.append(df_symbol.loc[ent.text]['CompanyName'])
         else:
@@ -59,117 +87,67 @@ def get_counts_df(text):
         counts_df = counts_df.groupby(0).index.count().reset_index().rename(columns={0: 'company', 'index': 'counts'})
     except KeyError:
         return None
+
     counts_df['symbol'] = counts_df.company.apply(lambda x: df_company_name.loc[x]['Symbol'])
     counts_df = counts_df[['company', 'symbol', 'counts']]
+    counts_df = counts_df.sort_values(by='counts', ascending=False)
 
     return counts_df
 
 
-def main():
-    print("Welcome to the MarketWatch Analyzer App!\n")
-    print("This app focuses on analyzing MarketWatch articles and is able to count every stock mentioned in the "
-          "article as well as perform sentiment analysis on the article.\n")
-    while True:
-        url = input("Enter a URL to begin analyzing: ")
-        print()
-        result = article_scraper(url)
-        if result is None:
-            print("Sorry! It looks like the URL or article is in an unsupported format. Please try another one.")
-            continue
+# Get the sentences that are rated as most negative and most positive.
+def get_polarizing_sentences(text):
+    doc = nlp(text)
 
-        title, body = result[0], result[1]
+    min_polarity = float('inf')
+    max_polarity = float('-inf')
+    min_sentence = None
+    max_sentence = None
 
-        # Get title counts
-        title_df = get_counts_df(title)
-        print("Stocks mentioned in the article title:")
-        print(title_df)
-
-        print()
-        # Get article counts
-        article_body = get_counts_df(body)
-        print("Stocks mentioned in the article body:")
-        print(article_body)
-
-        print()
-
-        response = input("Would you like to see advanced analyses (sentiment analysis)? Enter [y]/[n]: ")
-        print()
-        if response in ['y', 'yes', '[y]', 'Y', 'Yes', '[Y]']:
-            # Perform sentiment analysis
-            title_subjectivity = get_subjectivity(title)
-            print("Title subjectivity:", title_subjectivity)
-
-            title_sentiment = get_polarity(title)
-            print("Title polarity:", title_sentiment)
-
-            article_subjectivity = get_subjectivity(body)
-            print("Article subjectivity:", article_subjectivity)
-
-            article_sentiment = get_polarity(body)
-            print("Article polarity:", article_sentiment)
-
-            print()
-            print('-----------------------')
-            print("Additional information:")
-            print("The subjectivity score ranges from 0 to 1, with 0 being objective and 1 being subjective.")
-            print("Positive, neutral, and negative polarity scores represent the probability that the sentiment is "
-                  "positive, neutral, or negative, respectively. The compound score ranges from -1 to 1, with "
-                  "-1 being negative and 1 being positive.")
-            print('-----------------------')
-            print()
-
-        most_mentioned_company = article_body[article_body.counts == article_body.counts.max()].iloc[0]['company']
-
-        # Request teammate_service
-        with open('request.txt', 'w') as file:
-            file.write(most_mentioned_company)
-
-        # Wait for teammate_service to run
-        while True:
-            try:
-                with open('response.txt') as file:
-                    contents = file.read()
-                    break
-            except FileNotFoundError:
-                time.sleep(1)
-
-        with Image.open(contents) as im:
-            im.show()
-
-        response = input("Would you like to analyze another article? Enter [y]/[n]: ")
-        if response not in ['y', 'yes', '[y]', 'Y', 'Yes', '[Y]']:
-            print("Goodbye!")
-            break
+    for sent in doc.sents:
+        polarity = get_polarity(sent.text)['compound']
+        if polarity < min_polarity:
+            min_polarity = polarity
+            min_sentence = sent
+        if polarity > max_polarity:
+            max_polarity = polarity
+            max_sentence = sent
+        
+    return min_sentence, max_sentence
 
 
-if __name__ == '__main__':
-    # Setup df and stocks/indexes list
-    stops = {'A', 'RBC', 'two'}
+# Setup
 
-    # Data modified from spacy.pythonhumanities.com
-    df = pd.read_csv("data/stocks.tsv", sep='\t')
+# Stop words
+stops = {'A', 'RBC', 'two'}
 
-    symbols = df.Symbol.tolist()
-    companies = df.CompanyName.tolist()
+# stocks.tsv modified from spacy.pythonhumanities.com
+df = pd.read_csv("static/data/stocks.tsv", sep='\t')
 
-    # Above DataFrame modified so that Symbol/CompanyName is the index
-    # to facilitate efficient lookup
-    df_symbol = df.set_index('Symbol')
-    df_company_name = df.set_index('CompanyName')
+symbols = df.Symbol.tolist()
+companies = df.CompanyName.tolist()
 
-    # Set up spaCy with entity ruler to find stock symbols/companies
-    nlp = spacy.blank('en')
-    ruler = nlp.add_pipe("entity_ruler")
-    patterns = []
-    for symbol in symbols:
-        if symbol not in stops:
-            patterns.append({'label': 'Stock', 'pattern': symbol})
-    for company in companies:
-        if company not in stops:
-            patterns.append({'label': 'Company', 'pattern': company})
-    ruler.add_patterns(patterns)
+# DataFrame modified so that Symbol/CompanyName is the index to facilitate efficient lookup
+df_symbol = df.set_index('Symbol')
+df_company_name = df.set_index('CompanyName')
 
-    # Setup vader
-    vader = SentimentIntensityAnalyzer()
+df_symbol = df_symbol[['CompanyName']]
+# Some companies have multiple stock symbols. To handle this, we group by company name and set the `symbol` column to equal all stock symbols separated by commas
+# https://stackoverflow.com/questions/50422809/pandas-group-by-with-all-the-values-of-the-group-as-comma-separated
+df_company_name = df_company_name.groupby(df_company_name.index).Symbol.agg([('Symbol', ', '.join)])
 
-    main()
+# Set up spaCy with sentencizer and entity ruler to find stock symbols/companies
+nlp = spacy.blank('en')
+nlp.add_pipe('sentencizer')
+ruler = nlp.add_pipe("entity_ruler")
+patterns = []
+for symbol in symbols:
+    if symbol not in stops:
+        patterns.append({'label': 'Stock', 'pattern': symbol})
+for company in companies:
+    if company not in stops:
+        patterns.append({'label': 'Company', 'pattern': company})
+ruler.add_patterns(patterns)
+
+# Set up vader
+vader = SentimentIntensityAnalyzer()
